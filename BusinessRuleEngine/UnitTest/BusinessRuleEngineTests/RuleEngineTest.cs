1ff4f7f14d74977d293da2a44cb1ae7bce1c4093
Arvind Pathak <47774825+arvindabes@users.noreply.github.com>
2021-08-22 17:16:41 +0530

    UTC updated for new payment types
    UTC updated for new payment types

--- BusinessRuleEngine/UnitTest/BusinessRuleEngineTests/RuleEngineTest.cs
+++ BusinessRuleEngine/UnitTest/BusinessRuleEngineTests/RuleEngineTest.cs
@@ -95,5 +95,95 @@ namespace BusinessRuleEngineTests
             Assert.NotEqual(expectedResponse, actualResponse);
         }
         #endregion
+
+        #region MembershipPaymentType
+        [Fact]
+        public void WhenPaymenmtType_Membership_ShouldReturn_ActivateMembership()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.Membership.ToString();
+            string expectedResponse = "ActivateMembership";
+
+            //Act
+            var actualResponse = "ActivateMembership";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+
+        [Fact]
+        public void WhenPaymenmtType_Membership_ShouldReturn_ActivateMembership_NotGenerated()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.Membership.ToString();
+            string expectedResponse = "ActivateMembership";
+
+            //Act
+            var actualResponse = "ActivateMembership_NotGenerated";
+
+            //Assert
+            Assert.NotEqual(expectedResponse, actualResponse);
+        }
+        #endregion
+
+        #region UpgradeMembershipPaymentType
+        [Fact]
+        public void WhenPaymenmtType_UpgradeMembership_ShouldReturn_UpgradeMembership()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.UpgradeMembership.ToString();
+            string expectedResponse = "UpgradeMembership";
+
+            //Act
+            var actualResponse = "UpgradeMembership";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+
+        [Fact]
+        public void WhenPaymenmtType_Membership_ShouldReturn_UpgradeMembership_NotGenerated()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.UpgradeMembership.ToString();
+            string expectedResponse = "UpgradeMembership";
+
+            //Act
+            var actualResponse = "UpgradeMembership_NotGenerated";
+
+            //Assert
+            Assert.NotEqual(expectedResponse, actualResponse);
+        }
+        #endregion
+
+        #region MembershipOrUpgradePaymentType
+        [Fact]
+        public void WhenPaymenmtType_MembershipOrUpgrade_ShouldReturn_MembershipOrUpgrade()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.UpgradeOrMembership.ToString();
+            string expectedResponse = "MembershipOrUpgrade";
+
+            //Act
+            var actualResponse = "MembershipOrUpgrade";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+
+        [Fact]
+        public void WhenPaymenmtType_Membership_ShouldReturn_MembershipOrUpgrade_NotGenerated()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.UpgradeMembership.ToString();
+            string expectedResponse = "MembershipOrUpgrade";
+
+            //Act
+            var actualResponse = "UpgradeMembership_NotGenerated";
+
+            //Assert
+            Assert.NotEqual(expectedResponse, actualResponse);
+        }
+        #endregion
     }
 }
