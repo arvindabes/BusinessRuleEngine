4c1f942b3e6e4cb607ea2718b929fe8427fd52f7
Arvind Pathak <47774825+arvindabes@users.noreply.github.com>
2021-08-22 17:59:07 +0530

    message correction
    message correction

--- BusinessRuleEngine/BusinessRuleEngine/Implementation/LearningToSkiVideo.cs
+++ BusinessRuleEngine/BusinessRuleEngine/Implementation/LearningToSkiVideo.cs
@@ -1,16 +1,14 @@
 ﻿using BusinessRuleEngine.Interface;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BusinessRuleEngine.Implementation
-{    
+{
 
     public class LearningToSkiVideo : IOrderProcessing
     {
         public string ProcessOrder()
         {
-            Console.WriteLine("First Add Video To Packing Slip");
+            Console.WriteLine("First-Aid Video To Packing Slip");
 
             return "FirstAddVideoToPackingSlip";
         }
