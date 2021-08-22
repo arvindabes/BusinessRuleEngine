fb70f91ea88a288ee530b7f5e40a0ea7def6ffab
Arvind Pathak <47774825+arvindabes@users.noreply.github.com>
2021-08-22 17:26:28 +0530

    LearningToSki and Commission payment implementation
    LearningToSki and Commission payment implementation

---
+++ BusinessRuleEngine/BusinessRuleEngine/Implementation/GenerateCommissionPayment.cs
@@ -0,0 +1,15 @@
+﻿using BusinessRuleEngine.Interface;
+using System;
+
+namespace BusinessRuleEngine.Implementation
+{
+    public class GenerateCommissionPayment : IOrderProcessing
+    {
+        public string ProcessOrder()
+        {
+            Console.WriteLine("generate a commission payment to the agent");
+
+            return "CommissionPaymentToAgent";
+        }
+    }
+}
---
+++ BusinessRuleEngine/BusinessRuleEngine/Implementation/LearningToSkiVideo.cs
@@ -0,0 +1,19 @@
+﻿using BusinessRuleEngine.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.Implementation
+{    
+
+    public class LearningToSkiVideo : IOrderProcessing
+    {
+        public string ProcessOrder()
+        {
+            Console.WriteLine("First Add Video To Packing Slip");
+
+            return "FirstAddVideoToPackingSlip";
+        }
+    }
+}
+
