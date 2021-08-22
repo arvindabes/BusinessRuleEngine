6da2039e78c001c7ac5e1391a9eeb247c6170a87
Arvind Pathak <47774825+arvindabes@users.noreply.github.com>
2021-08-22 17:02:21 +0530

    test case updated for physical product and book payment types
    test case updated for physical product and book payment types

--- BusinessRuleEngine/UnitTest/BusinessRuleEngineTests/RuleEngineTest.cs
+++ BusinessRuleEngine/UnitTest/BusinessRuleEngineTests/RuleEngineTest.cs
@@ -1,3 +1,4 @@
+using BusinessRuleEngine;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -6,7 +7,7 @@ namespace BusinessRuleEngineTests
     public class RuleEngineTest
     {
         [Fact]
-        public async Task WhenPaymenmtType_NULL_ShouldReturnEmpty()
+        public void WhenPaymenmtType_NULL_ShouldReturnEmpty()
         {
             //Arrange
             string paymentType = string.Empty;
@@ -18,5 +19,81 @@ namespace BusinessRuleEngineTests
             Assert.NotNull(response);
             
         }
+
+        #region UnKnownPaymentType
+        [Fact]
+        public void WhenPaymenmtType_Unknown_ShouldReturn_InvalidPaymentType()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.Unknown.ToString();
+            string expectedResponse = "InvalidPaymentType";
+
+            //Act
+            var actualResponse = "InvalidPaymentType";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+        #endregion
+
+        #region PhysicalProductPaymentType
+        [Fact]
+        public void WhenPaymenmtType_PhysicalProduct_ShouldReturn_ShippingSlip()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.PhysicalProduct.ToString();
+            string expectedResponse = "ShippingSlipGenerated";
+
+            //Act
+            var actualResponse = "ShippingSlipGenerated";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+
+        [Fact]
+        public void WhenPaymenmtType_PhysicalProduct_ShouldReturn_ShippingSlip_NotGenerated()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.PhysicalProduct.ToString();
+            string expectedResponse = "ShippingSlipGenerated";
+
+            //Act
+            var actualResponse = "ShippingSlipNotGenerated";
+
+            //Assert
+            Assert.NotEqual(expectedResponse, actualResponse);
+        }
+        #endregion
+
+        #region BookPaymentType
+        [Fact]
+        public void WhenPaymenmtType_Book_ShouldReturn_DuplicateSlipForRoyalityDepartment()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.Book.ToString();
+            string expectedResponse = "DuplicatePackingSlip";
+
+            //Act
+            var actualResponse = "DuplicatePackingSlip";
+
+            //Assert
+            Assert.Equal(expectedResponse, actualResponse);
+        }
+
+        [Fact]
+        public void WhenPaymenmtType_Book_ShouldReturn_DuplicateSlipForRoyalityDepartment_NotGenerated()
+        {
+            //Arrange
+            string paymentType = PaymentTypes.Book.ToString();
+            string expectedResponse = "DuplicatePackingSlip";
+
+            //Act
+            var actualResponse = "DuplicatePackingSlip_NotGenerated";
+
+            //Assert
+            Assert.NotEqual(expectedResponse, actualResponse);
+        }
+        #endregion
     }
 }
